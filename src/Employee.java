b7071c7c8d22e46f7cc07fd779f1b3b0bcafe8ff
vachg <vachgen@yandex.ru>
2024-09-28 17:05:54 +0300

    CouseWork1

---
+++ src/Employee.java
@@ -0,0 +1,61 @@
+import java.util.Objects;
+
+public class Employee {
+    // Лоля класса
+    private String name;
+    private int department;
+    private int salary;
+    // Статическая переменная-счетчик для id
+    private static int index = 1;
+    // Поле id для каждого объекта класса
+    private int id;
+
+    public Employee(String name, int department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.id = index++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && id == employee.id && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, department, salary, id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d,  %s,  department=%d,  salary=%d", id, name, department, salary);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
 public class Main {
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee("Fedorova Maria Ivanovna", 5, 57000);
+        employee[1] = new Employee("Sidorova Elena Viktorovna", 4, 59000);
+        employee[2] = new Employee("Petrova Anna Andreevna", 3, 61000);
+        employee[3] = new Employee("Ivanova Irina Ivanovna", 2, 65000);
+        employee[4] = new Employee("Fedorov Fedor Fedorovich", 1, 70000);
+        employee[5] = new Employee("Novikov Nikolay Sergeevich", 5, 57500);
+        employee[6] = new Employee("Petrov Petr Petrovich", 4, 58000);
+        employee[7] = new Employee("Sokolov Sergey Sergevich", 3, 61500);
+        employee[8] = new Employee("Morozova Maria Grigorievna", 2, 64000);
+        employee[9] = new Employee("Popov Pavel Sergeevich", 1, 69000);
+        printListEmployee(employee);
+        System.out.println();
+        System.out.println("сумма затрат на ЗП в месяц: " + calculateSumSalary(employee));
+        System.out.println();
+        System.out.println("Сотрудника с минимальной ЗП: " + findMinSaiary(employee));
+        System.out.println();
+        System.out.println("Сотрудника с максимальной ЗП: " + findMaxSaiary(employee));
+        System.out.println();
+        System.out.println("Среднее значение зарплат за месяц: " + calculateAverageSalary(employee));
+        System.out.println();
+        printName(employee);
+    }
+
+    private static void printListEmployee(Employee[] employee) {
+        for (int i = 0; i < employee.length; i++) {
+            System.out.println(employee[i]);
+        }
+    }
+
+    private static int calculateSumSalary(Employee[] employees) {
+        int sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            sum += employees[i].getSalary();
+        }
+        return sum;
+    }
+
+    private static int findMinSaiary(Employee[] employees) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < employees.length; i++) {
+            if (min >= employees[i].getSalary()) {
+                min = employees[i].getSalary();
+            }
+        }
+        return min;
+    }
+
+    private static int findMaxSaiary(Employee[] employees) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < employees.length; i++) {
+            if (max <= employees[i].getSalary()) {
+                max = employees[i].getSalary();
+            }
+        }
+        return max;
+    }
+
+    private static int calculateAverageSalary(Employee[] employees) {
+        int sumAverage = calculateSumSalary(employees);
+        return sumAverage / employees.length;
+    }
+
+    public static void printName(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println("Name: " + employees[i].getName());
+        }
     }
 }
+
